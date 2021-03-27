c313d63c7d1688ae26e3c79155ded126b60052cb
mandlar <marko.andlar@novena.hr>
2021-03-27 12:31:40 +0100

    Added hr keyboard with layers Need to implement keyboard functionality

---
+++ Assets/!Assets/Scripts/Marko_Scripts/HR_OSK_Controller.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Keyboard
+{
+    public class HR_OSK_Controller : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup _lowercaseLayer;
+        [SerializeField] private CanvasGroup _uppercaseLayer;
+        [SerializeField] private CanvasGroup _numbersLayer;
+        private CanvasGroup _currentLayer;
+        [SerializeField] private TMP_Text _inputTarget;
+
+        private void Start()
+        {
+            ShowCanvasGroup(_lowercaseLayer);
+            HideCanvasGroup(_uppercaseLayer);
+            HideCanvasGroup(_numbersLayer);
+        }
+
+        public void ShowLowercase()
+        {
+            ShowLayer(_lowercaseLayer);
+        }
+
+        public void ShowUppercase()
+        {
+            ShowLayer(_uppercaseLayer);
+        }
+
+        public void ShowNumbers()
+        {
+            ShowLayer(_numbersLayer);
+        }
+
+        private void ShowLayer(CanvasGroup layer)
+        {
+            HideCanvasGroup(_currentLayer);
+            ShowCanvasGroup(layer);
+        }
+
+        private void HideCanvasGroup(CanvasGroup canvas)
+        {
+            canvas.alpha = 0;
+            canvas.interactable = false;
+            canvas.blocksRaycasts = false;
+        }
+
+        private void ShowCanvasGroup(CanvasGroup canvas)
+        {
+            _currentLayer = canvas;
+            canvas.alpha = 1;
+            canvas.interactable = true;
+            canvas.blocksRaycasts = true;
+        }
+    }
+}
+
